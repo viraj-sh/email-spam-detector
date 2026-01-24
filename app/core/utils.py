bffe10542ce8190d1b63fcd9d87c09a61b83aa78
viraj-sh <virajs45@proton.me>
2026-01-24 13:19:36 +0530

    feat(api): initialize FastAPI server with system endpoints
    - '/' root endpoint and '/health' health check end point with pydantic schema
    - Added core fucntions, requirements

---
+++ app/core/utils.py
@@ -0,0 +1,23 @@
+from typing import Any, Optional
+
+# ASCII COLORS
+RESET = "\033[0m"
+BOLD = "\033[1m"
+FG_RED = "\033[31m"
+FG_WHITE = "\033[97m"
+FG_GREEN = "\033[32m"
+FG_YELLOW = "\033[33m"
+
+
+def standard_response(
+    success: bool,
+    error_msg: Optional[str] = None,
+    data: Optional[Any] = None,
+    status_code: int | None = None,
+) -> dict[str, Any]:
+    return {
+        "success": bool(success),
+        "error": error_msg if not success else None,
+        "data": data if success else None,
+        "status_code": status_code,
+    }
---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.v1.system import router as system_router
+from core.logging import setup_logging
+
+logger = setup_logging(name="app", level="INFO")
+
+app = FastAPI(title="Email Spam Detector API v1")
+
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://localhost:8000",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logger.info("Application startup complete")
+
+# Include routers
+app.include_router(system_router, prefix="/api/v1")
