f2058f63fd29eb796eb71ac213a169a4ef53da52
Saksham-3175 <codesakshamcode@gmail.com>
2026-01-31 12:41:50 +0530

    refactor(paths): use dataset/ for data and save trained model artifacts under app/model

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,10 @@
 import json
 import joblib
 
-def save_label_mapping(path = 'models/label_mapping.joblib'):
+def save_label_mapping(path = 'app/model/label_mapping.joblib'):
     label_map = {0: 'ham', 1: 'spam'}
     joblib.dump(label_map, path)
 
-def save_metadata(metadata: dict, path='models/model_metadata.json'):
+def save_metadata(metadata: dict, path='app/model/model_metadata.json'):
     with open(path, 'w') as f:
         json.dump(metadata, f, indent=4)
